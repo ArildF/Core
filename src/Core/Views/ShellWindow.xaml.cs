4fbbb5c3fed496c1354fa0bdd5dcd9c6f58a24e5
Arild Fines <arild.fines@broadpark.no>
2011-12-30 22:24:16 +0100

    Initial commit. Shell window in place with gradient backgroundt

---
+++ src/Core/Views/ShellWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace Rogue.Core.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for ShellWindow.xaml
+    /// </summary>
+    public partial class ShellWindow
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
